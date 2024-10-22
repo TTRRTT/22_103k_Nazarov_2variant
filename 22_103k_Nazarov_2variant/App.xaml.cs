ca4b609d59e33dc9117c8c257f8a2d8888f411eb
Artyom <artem.natharov@gmail.com>
2024-10-23 06:40:23 +0700

    now init

---
+++ 22_103k_Nazarov_2variant/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _22_103k_Nazarov_2variant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 22_103k_Nazarov_2variant/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22_103k_Nazarov_2variant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool isVowel(char ch)
+        {
+            ch = char.ToLower(ch);
+            if (ch == 'a' || ch == 'e' || ch == 'u' || ch == 'i' || ch == 'o')
+                return true;
+            
+            return false;
+        }
+        private void HandleWrongInput()
+        {
+            tb_input.Background = Brushes.Red;
+            tb_input.Foreground = Brushes.White;
+
+            tb_longWord.Text = string.Empty;
+            tb_vowelsCount.Text = string.Empty;
+        }
+
+        private void tb_input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tb_input.Background == Brushes.White)
+                return;
+
+            tb_input.Background = Brushes.White;
+            tb_input.Foreground = Brushes.Black;
+        }
+
+        private void btn_execute_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_input.Text == string.Empty)
+            {
+                HandleWrongInput();
+                return;
+            }
+
+            int vowelCounter = 0;
+            string longestWord = "";
+
+            foreach (var str in tb_input.Text.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (var ch in str)
+                {
+                    if (isVowel(ch))
+                        vowelCounter++;
+                }
+
+                if (str.Length > longestWord.Length)
+                    longestWord = str;
+            }
+
+            tb_vowelsCount.Text = vowelCounter.ToString();
+            tb_longWord.Text = longestWord;
+        }
+    }
+}
